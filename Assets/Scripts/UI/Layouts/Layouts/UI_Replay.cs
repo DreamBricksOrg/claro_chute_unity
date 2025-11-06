2623ba15881cba7a7cc5e2374018f2a931210254
MSP-2253\Thiago Bertoni <thiago.bertoni@monks.com>
2025-11-06 12:20:56 -0300

    fix: clear replay texture onshow and onhide

--- Assets/Scripts/UI/Layouts/Layouts/UI_Replay.cs
+++ Assets/Scripts/UI/Layouts/Layouts/UI_Replay.cs
@@ -3,6 +3,7 @@ using DG.Tweening;
 using SimpleJSON;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class UI_Replay : UI
@@ -10,6 +11,13 @@ public class UI_Replay : UI
 	public VideoPlayer videoPlayer;
 	public CanvasGroup videoCanvasGroup;
 	public RectTransform loadingSpinner;
+	private RawImage videoRawImage;
+
+	internal override void Awake()
+	{
+		base.Awake();
+		videoRawImage = videoPlayer.GetComponent<RawImage>();
+	}
 
 	internal override void Start()
 	{
@@ -41,12 +49,23 @@ public class UI_Replay : UI
 	{
 		base.OnShow(data, callback);
 		loadingSpinner.DORotate(new Vector3(0, 0, -360), 1f, RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);
+		ClearRenderTexture();
 	}
 
 	override internal void OnHide(object data, Action<object> callback)
 	{
 		base.OnHide(data, callback);
 		loadingSpinner.DOKill();
+		ClearRenderTexture();
+	}
+
+	public void ClearRenderTexture()
+	{
+		var rt = videoRawImage.texture as RenderTexture;
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = rt;
+        GL.Clear(true, true, new Color(0f, 0f, 0f, 0f));
+        RenderTexture.active = currentRT;
 	}
 
 	private void OnVideoReplayCompleted(string url)
@@ -63,15 +82,15 @@ public class UI_Replay : UI
 	private void OnVideoPrepared(VideoPlayer source)
 	{
 		Debug.Log("<<PLAY>>");
-		if( Main.Instance.currentSectionType != SectionTypes.Replay ) return;
+		if (Main.Instance.currentSectionType != SectionTypes.Replay) return;
 		Play();
 	}
-	
+
 	void Play()
-    {
+	{
 		videoPlayer.Play();
 		videoCanvasGroup.DOFade(1f, 0.3f).SetEase(Ease.Linear);
-    }
+	}
 
 	private void OnVideoEnd(VideoPlayer source)
 	{
